e816f189fbcdcf65ab90574eb35af9f3a96a84b6
Yohendry Hurtado <yohendry.hurtado@gmail.com>
2015-04-22 12:22:27 -0500

    About Enums

--- koans/src/beginner/AboutEnums.java
+++ koans/src/beginner/AboutEnums.java
@@ -8,36 +8,39 @@ public class AboutEnums {
 
 
 	enum Colors {
-		Red, Blue, Green, Yellow // what happens if you add a ; here?
+		Red, Blue, Green, Yellow; // what happens if you add a ; here?
 		// What happens if you type Red() instead?
 	}
 	
 	@Koan
 	public void basicEnums() {
 		Colors blue = Colors.Blue;
-		assertEquals(blue == Colors.Blue, __);
-		assertEquals(blue == Colors.Red, __);
-		assertEquals(blue instanceof Colors, __);
+		assertEquals(blue == Colors.Blue, true);
+		assertEquals(blue == Colors.Red, false);
+		assertEquals(blue instanceof Colors, true);
 	}
 	
 	@Koan
 	public void basicEnumsAccess() {
 		Colors[] colorArray = Colors.values();
-		assertEquals(colorArray[2], __);
+		assertEquals(colorArray[2], Colors.Green);
 	}
-	
+
 	enum SkatSuits {
 		Clubs(12), Spades(11), Hearts(10), Diamonds(9);
 		SkatSuits(int v) { value = v; }
 		private int value;
 	}
+
 	
 	@Koan
 	public void enumsWithAttributes() {
 		// value is private but we still can access it. Why?
+		// because the enum its decared inside the AboutEnums class
 		// Try moving the enum outside the AboutEnum class... What do you expect?
+		// it raise a compile error because can not access to value variable
 		// What happens?
-		assertEquals(SkatSuits.Clubs.value > SkatSuits.Spades.value, __);
+		assertEquals(SkatSuits.Clubs.value > SkatSuits.Spades.value, true);
 	}
 	
 	enum OpticalMedia {
@@ -53,7 +56,8 @@ public class AboutEnums {
 	
 	@Koan
 	public void enumsWithMethods() {
-		assertEquals(OpticalMedia.CD.getCoolnessFactor(), __);
-		assertEquals(OpticalMedia.BluRay.getCoolnessFactor(), __);
+		assertEquals(OpticalMedia.CD.getCoolnessFactor(), -3500);
+		assertEquals(OpticalMedia.BluRay.getCoolnessFactor(), 490000);
 	}
 }
+
